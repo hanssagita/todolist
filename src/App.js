207e76a7c454d84c7a27e8d76dfb5b29c0992945
GDN\hans.sagita <hans.sagita@gdn-commerce.com>
2018-10-05 00:28:14 +0700

    Prepare All the file project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/images/logo.svg';
+import styled, { keyframes } from 'styled-components'
+
+const Container = styled.div`
+  text-align: center;
+`
+
+const Header = styled.div`
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`
+
+const logoSpin = keyframes`
+  from { transform: rotate(0deg); }
+  to { transform: rotate(360deg); }
+`
+
+const Logo = styled.img`
+  animation: ${logoSpin} infinite 20s linear;
+  height: 80px;
+`
+
+const Title = styled.h1 `
+  font-size: 1.5em;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Container>
+        <Header>
+          <Logo src={logo} alt="logo" />
+          <Title>Welcome to React</Title>
+        </Header>
+      </Container>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { injectGlobal } from 'styled-components'
+
+injectGlobal`
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+`
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
